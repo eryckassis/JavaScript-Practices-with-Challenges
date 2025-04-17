29ccfbcdb8249935ac1ba15e3df59573a9750658
Eryck Assis <eng.assis.dev@gmail.com>
2025-04-16 23:00:00 -0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function sortear() {
-  let quantidade = document.getElementById("quantidade").value;
-  let de = document.getElementById("de").value;
-  let ate = document.getElementById("ate").value;
-
-  alert(`Quantidade: ${quantidade}`);
-  alert(`Do número: ${de}`);
-  alert(`Quantidade: ${ate}`);
-}
